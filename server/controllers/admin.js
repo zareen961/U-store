47d68f41a281918f1e124edc1e30c55bdcb8114f
kumarsks619 <kumarsks619@gmail.com>
2021-04-08 00:21:51 +0530

    User & Admin partially DONE

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,13 +1,60 @@
 const asyncHandler = require('express-async-handler')
 
+const Admin = require('../models/Admin')
+
 // to register a new admin
-const adminRegister = asyncHandler(async (req, res) => {})
+const adminRegister = asyncHandler(async (req, res) => {
+    const { username, password } = req.body
+
+    const newAdmin = await Admin.create({
+        username,
+        password,
+    })
+
+    if (newAdmin) {
+        res.status(201).json(newAdmin)
+    } else {
+        res.status(500)
+    }
+})
 
 // to login an existing admin
-const adminLogin = asyncHandler(async (req, res) => {})
+const adminLogin = asyncHandler(async (req, res) => {
+    const { username, password } = req.body
+
+    // finding the admin
+    const foundAdmin = await Admin.findOne({ username })
+
+    if (foundAdmin && (await foundAdmin.matchPassword(password))) {
+        res.send({
+            username: foundAdmin.username,
+            token: generateToken(foundAdmin._id),
+        })
+    } else {
+        res.status(401)
+        throw new Error('Wrong Credentials!')
+    }
+})
 
 // to delete an admin
-const adminDelete = asyncHandler(async (req, res) => {})
+const adminDelete = asyncHandler(async (req, res) => {
+    const adminID = req.params.adminID
+    const { password } = req.body
+
+    // finding the logged in admin
+    const foundAdmin = await Admin.findById(req.authAdmin._id)
+
+    if (foundAdmin && (await foundAdmin.matchPassword(password))) {
+        const foundAdminToDelete = await Admin.findById(adminID)
+
+        await foundAdminToDelete.remove()
+
+        res.status(200).json({ message: 'Admin Deleted!', adminID })
+    } else {
+        res.status(401)
+        throw new Error('Wrong Credentials!')
+    }
+})
 
 // to add a new state
 const adminStateAdd = asyncHandler(async (req, res) => {})
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,15 +1,117 @@
 const asyncHandler = require('express-async-handler')
 
+const User = require('../models/User')
+
 // to register new user
-const userRegister = asyncHandler(async (req, res) => {})
+const userRegister = asyncHandler(async (req, res) => {
+    const {
+        email,
+        username,
+        firstName,
+        lastName,
+        avatar,
+        primaryPhone,
+        secondaryPhone,
+        collegeState,
+        collegeCity,
+        college,
+        password,
+    } = req.body
+
+    const newUser = await User.create({
+        email,
+        username,
+        firstName,
+        lastName,
+        avatar,
+        primaryPhone,
+        secondaryPhone,
+        collegeState,
+        collegeCity,
+        college,
+        password,
+    })
+
+    if (newUser) {
+        res.status(201).json('New User Registered!')
+    } else {
+        res.status(500)
+        throw new Error('Invalid User data!')
+    }
+})
 
 // to login existing user
-const userLogin = asyncHandler(async (req, res) => {})
+const userLogin = asyncHandler(async (req, res) => {
+    const { usernameOrEmail, password } = req.body
+
+    // finding the user by either username or email
+    const foundUser = await User.findOne({
+        $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }],
+    })
+
+    if (foundUser && (await foundUser.matchPassword(password))) {
+        res.send({
+            ...foundUser,
+            token: generateToken(foundAdmin._id),
+        })
+    } else {
+        res.status(401)
+        throw new Error('Wrong Credentials!')
+    }
+})
 
 // to delete existing user
-const userDelete = asyncHandler(async (req, res) => {})
+const userDelete = asyncHandler(async (req, res) => {
+    const { password } = req.body
+
+    // finding the user
+    const foundUser = await findById(req.authUser._id)
+
+    if (foundUser && (await foundUser.matchPassword(password))) {
+        await foundUser.remove()
+        res.status(200).json({
+            message: 'User Deleted!',
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid Credentials!')
+    }
+})
 
 // to update details of existing user
-const userUpdate = asyncHandler(async (req, res) => {})
+const userUpdate = asyncHandler(async (req, res) => {
+    const {
+        email,
+        username,
+        firstName,
+        lastName,
+        avatar,
+        primaryPhone,
+        secondaryPhone,
+        collegeState,
+        collegeCity,
+        college,
+        password,
+    } = req.body
+
+    // finding the user
+    const foundUser = await findById(req.authUser._id)
+
+    foundUser.email = email
+    foundUser.username = username
+    foundUser.firstName = firstName
+    foundUser.lastName = lastName
+    foundUser.avatar = avatar
+    foundUser.primaryPhone = primaryPhone
+    foundUser.secondaryPhone = secondaryPhone
+    foundUser.collegeState = collegeState
+    foundUser.collegeCity = collegeCity
+    foundUser.college = college
+    foundUser.password = password
+
+    await foundUser.save()
+
+    res.status(200).json(foundUser)
+})
 
 module.exports = { userRegister, userLogin, userDelete, userUpdate }
