c17e79b20b42574928cf40d83e83a60721f43c2e
kumarsks619 <kumarsks619@gmail.com>
2021-04-12 01:48:36 +0530

    Axios Instance ADDED

---
+++ client/src/utils/axiosInstance.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+// const SERVER_URL = 'http://localhost:5000'
+const SERVER_URL = 'https://u-store-app.herokuapp.com'
+
+const axiosInstance = axios.create({
+    baseURL: SERVER_URL,
+})
+
+export default axiosInstance
