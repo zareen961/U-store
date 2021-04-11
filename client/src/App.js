9ed46dad5aaf4418c5f515d72d0c58bccf658c7e
Ifa Zareen <z1reen.961@gmail.com>
2021-04-11 20:57:51 +0530

    client src redux store SETUP

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css"
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ client/src/store/actionTypes.js
@@ -0,0 +1,54 @@
+// ALERT ***************************************************************************************
+export const ALERT_ADD = "ALERT_ADD"
+export const ALERT_REMOVE = "ALERT_REMOVE"
+
+// USER ****************************************************************************************
+export const USER_REGISTER_REQUEST = "USER_REGISTER_REQUEST"
+export const USER_REGISTER_SUCCESS = "USER_REGISTER_SUCCESS"
+export const USER_REGISTER_FAIL = "USER_REGISTER_FAIL"
+
+export const USER_LOGIN_REQUEST = "USER_LOGIN_REQUEST"
+export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS"
+export const USER_LOGIN_FAIL = "USER_LOGIN_FAIL"
+
+export const USER_UPDATE_REQUEST = "USER_UPDATE_REQUEST"
+export const USER_UPDATE_SUCCESS = "USER_UPDATE_SUCCESS"
+export const USER_UPDATE_FAIL = "USER_UPDATE_FAIL"
+
+export const USER_DELETE_REQUEST = "USER_DELETE_REQUEST"
+export const USER_DELETE_SUCCESS = "USER_DELETE_SUCCESS"
+export const USER_DELETE_FAIL = "USER_DELETE_FAIL"
+
+export const COLLEGE_FETCH_ALL_REQUEST = "COLLEGE_FETCH_ALL_REQUEST"
+export const COLLEGE_FETCH_ALL_SUCCESS = "COLLEGE_FETCH_ALL_SUCCESS"
+export const COLLEGE_FETCH_ALL_FAIL = "COLLEGE_FETCH_ALL_FAIL"
+
+// PRODUCT **************************************************************************************
+export const PRODUCT_FETCH_ALL_REQUEST = "PRODUCT_FETCH_ALL_REQUEST"
+export const PRODUCT_FETCH_ALL_SUCCESS = "PRODUCT_FETCH_ALL_SUCCESS"
+export const PRODUCT_FETCH_ALL_FAIL = "PRODUCT_FETCH_ALL_FAIL"
+
+export const PRODUCT_UPLOAD_REQUEST = "PRODUCT_UPLOAD_REQUEST"
+export const PRODUCT_UPLOAD_SUCCESS = "PRODUCT_UPLOAD_SUCCESS"
+export const PRODUCT_UPLOAD_FAIL = "PRODUCT_UPLOAD_FAIL"
+
+export const PRODUCT_DELETE_REQUEST = "PRODUCT_DELETE_REQUEST"
+export const PRODUCT_DELETE_SUCCESS = "PRODUCT_DELETE_SUCCESS"
+export const PRODUCT_DELETE_FAIL = "PRODUCT_DELETE_FAIL"
+
+export const PRODUCT_EDIT_REQUEST = "PRODUCT_EDIT_REQUEST"
+export const PRODUCT_EDIT_SUCCESS = "PRODUCT_EDIT_SUCCESS"
+export const PRODUCT_EDIT_FAIL = "PRODUCT_EDIT_FAIL"
+
+// BID ******************************************************************************************
+export const BID_PLACE_REQUEST = "BID_PLACE_REQUEST"
+export const BID_PLACE_SUCCESS = "BID_PLACE_SUCCESS"
+export const BID_PLACE_FAIL = "BID_PLACE_FAIL"
+
+export const BID_DELETE_REQUEST = "BID_DELETE_REQUEST"
+export const BID_DELETE_SUCCESS = "BID_DELETE_SUCCESS"
+export const BID_DELETE_FAIL = "BID_DELETE_FAIL"
+
+export const BID_STATUS_UPDATE_REQUEST = "BID_STATUS_UPDATE_REQUEST"
+export const BID_STATUS_UPDATE_SUCCESS = "BID_STATUS_UPDATE_SUCCESS"
+export const BID_STATUS_UPDATE_FAIL = "BID_STATUS_UPDATE_FAIL"
