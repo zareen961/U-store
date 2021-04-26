ed0022d2d55a5e6f616cce78d6243a742678b5c8
kumarsks619 <kumarsks619@gmail.com>
2021-04-27 01:02:56 +0530

    Working on Overview

---
+++ client/src/components/Landing/Overview/OverviewItem/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './OverviewItem.css'
+
+const OverviewItem = () => {
+    return (
+        <div>
+            <h1>Overview Item</h1>
+        </div>
+    )
+}
+
+export default OverviewItem
