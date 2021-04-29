f9442559814d78eb65538a1b1f902bf12421e7ad
Ifa Zareen <z1reen.961@gmail.com>
2021-04-30 00:41:26 +0530

    Working on FAQ Accordion

---
+++ client/src/components/Landing/ContactUs/FAQ/Accordion/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+import IconButton from '@material-ui/core/IconButton'
+
+import './Accordion.css'
+
+const Accordion = ({ query }) => {
+    const [show, setShow] = useState(false)
+
+    const handleShow = () => {
+        setShow(!show)
+    }
+
+    return (
+        <div className="accordion">
+            <div className="accordion__header">
+                <h3>{query.title}</h3>
+                <IconButton className="accordion__showButton" onClick={handleShow}>
+                    <AddBoxIcon />
+                </IconButton>
+            </div>
+
+            <p className={show && 'show'}>{query.answer}</p>
+        </div>
+    )
+}
+
+export default Accordion
