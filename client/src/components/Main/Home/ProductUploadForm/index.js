fb5ad458562d576e50a34f58014262b7ce12c8ee
Ifa Zareen <z1reen.961@gmail.com>
2021-05-29 12:43:18 +0530

    Working on Product Upload Form

--- client/src/components/Main/Home/ProductUploadForm/index.js
+++ client/src/components/Main/Home/ProductUploadForm/index.js
@@ -1,12 +1,42 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import { TagIcon } from '@primer/octicons-react'
 
+import sampleProduct from '../../../../assets/images/sample-product.jpg'
+import ButtonComp from '../../../utils/ButtonComp'
 import './ProductUploadForm.css'
 
 const ProductUploadForm = () => {
     return (
-        <div>
-            <h1>ProductUploadForm</h1>
-        </div>
+        <form className="productUploadForm">
+            <Avatar src="avatars/avatar10.png" className="productUploadForm__avatar" />
+            <input
+                type="text"
+                placeholder="What do you want to sell?"
+                className="productUploadForm__name"
+            />
+            <input
+                type="number"
+                placeholder="Set your price."
+                className="productUploadForm__price"
+            />
+            <textarea
+                placeholder="Tell something about your product!"
+                className="productUploadForm__description"
+            ></textarea>
+            <div className="productUploadForm__imageWrapper">
+                <img src={sampleProduct} alt="sample-product" onClick={() => {}} />
+            </div>
+
+            <ButtonComp
+                typeClass={'primary'}
+                modifyClass={'iconButton uploadButton'}
+                handleOnClick={() => {}}
+                text={'Upload'}
+            >
+                <TagIcon size={18} />
+            </ButtonComp>
+        </form>
     )
 }
 
