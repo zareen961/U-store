478da137843a18eb86eab22d2877534903f32c03
VeNoM <kumarsks619@gmail.com>
2021-10-18 00:34:35 +0530

    Readmes UPDATED and new readme in server ADDED | Client - start-local.sh REMOVED

--- client/src/constants/vars.js
+++ client/src/constants/vars.js
@@ -1,4 +1,6 @@
-// export const NODE_ENV = 'development'
-export const NODE_ENV = 'production'
+// export const NODE_ENV = 'DEV'
+export const NODE_ENV = 'PROD'
 export const DEBOUNCE_TIME_IN_MILLISECONDS = 2000
 export const CACHE_TIME_IN_MINUTES = 10
+export const VAPID_KEY =
+    'BC8ZYTJyGmJwT7DuoYhFtBS8knKwgF4u-UDbmv1PdX823wUNP-q-dsR8_F9jfdbupgYat-gSWKcXIISX8A8dnPs'
